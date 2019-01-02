ceb361bdb50396d28a0bbdd518844c758af8f6b5
Faiz <891366402@qq.com>
2019-01-02 20:03:57 +0800

    为啥有错，获取在看看

--- profile/profile.go
+++ profile/profile.go
@@ -1,1 +1,21 @@
 package profile
+
+import (
+	"github.com/alfredyang1986/blackmirror/bmmodel"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Profile struct {
+	Id       string        `json:"id"`
+	Id_      bson.ObjectId `bson:"_id"`
+	Username string        `json:"username" bson:"username"`
+	// Password string        `json:"Company" jsonapi:"relationships"`
+}
+
+func (bd *Profile) ResetIdWithId_() {
+	bmmodel.ResetIdWithId_(bd)
+}
+
+func (bd *Profile) RestId_WithID() {
+	bmmodel.ResetId_WithID(bd)
+}
