68cf76ea765652271122b5e678c9f385e32af364
Faiz <891366402@qq.com>
2019-01-02 20:07:50 +0800

    哎呀，有点门道了

--- profile/profile.go
+++ profile/profile.go
@@ -2,6 +2,7 @@ package profile
 
 import (
 	"github.com/alfredyang1986/blackmirror/bmmodel"
+	"github.com/alfredyang1986/blackmirror/bmmodel/request"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -18,4 +19,40 @@ func (bd *Profile) ResetIdWithId_() {
 
 func (bd *Profile) RestId_WithID() {
 	bmmodel.ResetId_WithID(bd)
+}
+
+/*------------------------------------------------
+ * bmobject interface
+ *------------------------------------------------*/
+
+func (bd *Profile) QueryObjectId() bson.ObjectId {
+	return bd.Id_
+}
+
+func (bd *Profile) QueryId() string {
+	return bd.Id
+}
+
+func (bd *Profile) SetObjectId(id_ bson.ObjectId) {
+	bd.Id_ = id_
+}
+
+func (bd *Profile) SetId(id string) {
+	bd.Id = id
+}
+
+/*------------------------------------------------
+ * mongo interface
+ *------------------------------------------------*/
+
+func (bd *Profile) InsertBMObject() error {
+	return bmmodel.InsertBMObject(bd)
+}
+
+func (bd *Profile) FindOne(req request.Request) error {
+	return bmmodel.FindOne(req, bd)
+}
+
+func (bd *Profile) UpdateBMObject(req request.Request) error {
+	return bmmodel.UpdateOne(req, bd)
 }
